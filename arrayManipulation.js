c3db6cb3e0ab120f39fa209530e88bf85f0674f5
Owusu Ansah Joy <owusujoyansah@gmail.com>
2024-05-17 00:31:28 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,5 @@
+// Task 1
+
 function processArray(arr) {
     return arr.map(num => {
         if (num % 2 === 0) {
@@ -11,3 +13,21 @@ function processArray(arr) {
 const numbers = [1, 2, 3, 4, 5];
 const processedNumbers = processArray(numbers);
 console.log(processedNumbers);
+
+// Task 2
+
+
+function formatArrayStrings(stringArray, numberArray) {
+    return stringArray.map((str, index) => {
+        if (numberArray[index] % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+}
+
+const stringArray = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+const numberArray = processArray([1, 2, 3, 4, 5]);
+const formattedStrings = formatArrayStrings(stringArray, numberArray);
+console.log(formattedStrings);
